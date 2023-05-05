f7cb084df7a25f4ef6230f9d3a11ebaf929be025
emir <95132095+wearus@users.noreply.github.com>
2023-05-05 18:07:25 +0300

    Update app.js

--- node/src/app.js
+++ node/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const cors = require('cors');
-const { Client } = require('discord.js');
 
 const app = express();
 
@@ -9,52 +8,22 @@ app.use(cors());
 
 app.get('/download/:id', async (req, res, next) => {
   try {
-    const id = req.params.id; // parametre olarak id alıyoruz
-    const videoUrl = `https://youtu.be/${id}`; // youtube url'ini oluşturuyoruz
-    const videoInfo = await ytdl.getInfo(videoUrl);
-    const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+    const id = req.params.id; // getting the id as parameter
+    const videoUrl = `https://youtu.be/${id}`; // creating the youtube url
+    const videoInfo = await ytdl.getInfo(videoUrl); // getting the video information
+    const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' }); // getting the audio
     
+    // setting the headers
     res.header('Content-Type', 'audio/mpeg');
     res.header('Content-Disposition', `attachment; filename="${videoInfo.title}.mp3"`);
 
+    // streaming the audio
     ytdl(videoUrl, {
       quality: audioFormat.itag
     }).pipe(res);
   } catch (err) {
     next(err);
   }
-});
-
-// discord botu
-const client = new Client();
-client.login('BOT_TOKEN');
-
-// stream için route
-app.get('/avatar/:userId', (req, res) => {
-    const userId = req.params.userId;
-    // user idsinden avatarı çekiyoruz
-    client.users.fetch(userId)
-    .then(user => {
-        // user bulunamadıysa 404 döndürüyoruz
-        if (!user) {
-            res.status(404).send('User not found');
-            return;
-        }
-
-        // headerları ayarlıyoruz
-        res.set({
-            'Content-Type': 'image/png',
-            'Content-Disposition': 'attachment; filename=avatar.png',
-        });
-
-        // avatarı stream ediyoruz
-        const avatar = user.displayAvatarURL({ format: 'png', size: 512 }).pipe(res);
-    })
-    // hata varsa 404 döndürüyoruz
-    .catch(err => {
-        res.status(404).send('User not found');
-    }
-    );
 });
 
 let port = process.env.PORT || 3000;
