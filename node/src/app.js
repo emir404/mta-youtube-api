5a74cd41f9f17a92c21ad262a772355ac09f7c1c
emir <reostraemir@gmail.com>
2023-06-21 22:54:01 +0300

    memory update

--- node/src/app.js
+++ node/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
 const cors = require('cors');
 
 const app = express();
@@ -9,24 +9,24 @@ app.use(cors());
 app.get('/download/:id', async (req, res, next) => {
   try {
     const id = req.params.id; // getting the id as parameter
-    const videoUrl = `https://youtu.be/${id}`; // creating the youtube url
+    const videoUrl = `https://youtu.be/${id}`; // creating the YouTube URL
     const videoInfo = await ytdl.getInfo(videoUrl); // getting the video information
-    const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' }); // getting the audio
-    
+    const audioFormat = ytdl.filterFormats(videoInfo.formats, { quality: 'highestaudio' })[0]; // getting the audio format
+
     // setting the headers
     res.header('Content-Type', 'audio/mpeg');
     res.header('Content-Disposition', `attachment; filename="${videoInfo.title}.mp3"`);
 
     // streaming the audio
     ytdl(videoUrl, {
-      quality: audioFormat.itag
+      quality: audioFormat.itag,
     }).pipe(res);
   } catch (err) {
     next(err);
   }
 });
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`${port} portunda sunucu başlatıldı...`);
+  console.log(`Server started on port ${port}`);
 });
